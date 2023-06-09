f71a109c18b72e4b43b9330af30e5d428e33be86
Thijsvandepoll <thijsvandepoll@gmail.com>
2023-06-09 10:46:46 +0200

    Changed names in setup

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="many",
-    version="0.0.1",
+    name="many-migrations",
+    version="0.1",
     description="A library to create a customized migration application.",
     author="Thijs van de Poll",
     author_email="thijsvandepoll@gmail.com",
