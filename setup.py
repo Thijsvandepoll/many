8bbe84696eee6e0528f6765740209eb6eb0cb4bb
Thijsvandepoll <thijsvandepoll@gmail.com>
2023-06-09 11:37:58 +0200

    Updated versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="many-migrations",
-    version="0.1",
+    version="0.2",
     description="A library to create a customized migration application.",
     author="Thijs van de Poll",
     author_email="thijsvandepoll@gmail.com",
